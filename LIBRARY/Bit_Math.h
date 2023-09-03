445d1dce2c56e2a66b678d5839518c992c353f64
MasameEh <samehms116@gmail.com>
2023-09-03 06:26:28 +0300

    add guards
    Signed-off-by: MasameEh <samehms116@gmail.com>

--- LIBRARY/Bit_Math.h
+++ LIBRARY/Bit_Math.h
@@ -1,5 +1,16 @@
+/* 
+ * File:   Bit_Math.h
+ *
+ * Author: Mohamed Sameh
+ *
+ */
 
+#ifndef BIT_MATH_H_
+#define BIT_MATH_H_
 
+/* Section : Includes */
+
+/* Section : Macro Declarations */
 #define REGISTER_SIZE 8
 #define SET_BIT(REG,BIT_POS)    (REG|=(1<<BIT_POS))
 #define CLR_BIT(REG,BIT_POS)    (REG&=(~(1<<BIT_POS)))
@@ -8,3 +19,12 @@
 #define IS_BIT_SET(REG,BIT_POS)  ((REG&(1<<BIT_POS))>>BIT_POS)
 #define IS_BIT_CLR(REG,BIT_POS)  (!((REG&(1<<BIT_POS))>>BIT_POS))
 
+/* Section : Macro Functions Declarations */
+
+
+/* Section : Data Types Declarations  */
+
+
+/* Section : Functions Declarations */
+
+#endif
